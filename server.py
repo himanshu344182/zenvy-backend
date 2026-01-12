2fa6136bcd4dfff7f9badff9eb9204113c029a8a
Himanshu Arora <ha344182@gmail.com>
2026-01-13 02:47:20 +0530

    fix CORS for custom domain zenvy.biz

--- server.py
+++ server.py
@@ -672,8 +672,11 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=[
+        "https://zenvy.biz",
+        "https://www.zenvy.biz",
+    ],
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
